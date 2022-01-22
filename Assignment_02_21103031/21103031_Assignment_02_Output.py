3474bce96931527cfc54270fb396e93c11f0820a
AnshRajSharma <97468572+AnshRajSharma@users.noreply.github.com>
2022-01-22 21:45:14 +0530

    Add files via upload

---
+++ Assignment_02_21103031/21103031_Assignment_02_Output.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_1.py ==========
+
+The given string : Python is a case sensitive language
+
+a. The length of the given string : 35
+b. The reverse of the given string : egaugnal evitisnes esac a si nohtyP
+c. The desired string after slicing the given string : a case sensitive
+d. The desired replaced string : Python is object oriented language
+e. The index of substring “a” in the given string : 10
+f. The string after removing white spaces from given string : Pythonisacasesensitivelanguage 
+
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_2.py ==========
+
+The required formatted string is :
+Hey, ANSH RAJ SHARMA Here!
+My SID is 21103031
+I am from Computer Science and Engineering department and my CGPA is 9.5.
+
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_3.py ==========
+
+The given values are a = 56 and b = 10.
+
+a. Bitwise AND (a,b) Output : 8
+b. Bitwise OR (a,b) Output : 58
+c. Bitwise XOR (a,b) Output : 50
+d. Bitwise Left shift
+   Left shift 56 with 2 bits : 224
+   Left shift 10 with 2 bits : 40
+e. Bitwise Right shift
+   Right shift 56 with 2 bits : 14
+   Right shift 10 with 4 bits : 0 
+
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_4.py ==========
+
+Enter integer value 1 : 20
+Enter integer value 2 : 30
+Enter integer value 3 : 10
+The greatest of the three integer numbers is : 30 
+
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_5.py ==========
+
+Enter some text (string) : A python program to check name word here.
+YES the "name" substring is there in the input text. 
+
+
+========== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_02\Question_6.py ==========
+
+Enter the first side length of triangle (integer) : 14
+Enter the second side length of triangle (integer) : 15
+Enter the third side length of triangle (integer) : 16
+
+YES the triangle with such sides can be formed.
---
+++ Assignment_02_21103031/21103031_Assignment_02_SourceCode.py
@@ -0,0 +1,105 @@
+# Question 01
+# Program to utilize various built-in string functions or methods.
+
+given_string = "Python is a case sensitive language"
+
+print("\n", "The given string : ", given_string, "\n", sep='')
+
+print("a. The length of the given string :", len(given_string))
+
+print("b. The reverse of the given string :", given_string[::-1])
+
+new_string = given_string[10:26]
+print("c. The desired string after slicing the given string :", new_string)
+
+replaced_string = given_string.replace("a case sensitive", "object oriented")
+print("d. The desired replaced string :", replaced_string)
+
+index_value = given_string.index("a")
+print("e. The index of substring “a” in the given string :", index_value)
+
+modified_string = given_string.replace(" ", "")
+print("f. The string after removing white spaces from given string :", modified_string, "\n")
+
+# Question 02
+# Program to implement string formatting techniques in python.
+
+student_name = "ANSH RAJ SHARMA"
+student_ID = 21103031
+department_name = 'Computer Science and Engineering'
+student_CGPA = 9.5
+
+formatted_string = "Hey, {} Here!\n" \
+                   "My SID is {}\n" \
+                   "I am from {} department and my CGPA is {}." \
+                   .format(student_name, student_ID, department_name, student_CGPA)
+
+print("\n", "The required formatted string is :\n", formatted_string, "\n", sep='')
+
+# Question 03
+# Program to make use of bitwise operators.
+
+var_a = 56
+var_b = 10
+
+print("\n", "The given values are a = {} and b = {}.".format(var_a, var_b), "\n", sep='')
+
+print("a. Bitwise AND (a,b) Output :", var_a & var_b)
+
+print("b. Bitwise OR (a,b) Output :", var_a | var_b)
+
+print("c. Bitwise XOR (a,b) Output :", var_a ^ var_b)
+
+print("d. Bitwise Left shift")
+print("   Left shift {} with 2 bits : {}".format(var_a, var_a << 2))
+print("   Left shift {} with 2 bits : {}".format(var_b, var_b << 2))
+
+print("e. Bitwise Right shift")
+print("   Right shift {} with 2 bits : {}".format(var_a, var_a >> 2))
+print("   Right shift {} with 4 bits : {}".format(var_b, var_b >> 4), "\n")
+
+# Question 04
+# Program to find the greatest of three numbers entered by the user.
+
+num1 = int(input("\nEnter integer value 1 : "))
+num2 = int(input("Enter integer value 2 : "))
+num3 = int(input("Enter integer value 3 : "))
+
+if (num1 >= num2) and (num1 >= num3):
+    greatest_num = num1
+
+elif (num2 >= num1) and (num2 >= num3):
+    greatest_num = num2
+
+else:
+    greatest_num = num3
+
+print("The greatest of the three integer numbers is :", greatest_num, "\n")
+
+# Question 05
+# Program to check if the word “name” is present in the string entered by the user.
+
+input_string = input("\nEnter some text (string) : ")
+
+if "name" in input_string:
+    print("YES the \"name\" substring is there in the input text.", "\n")
+
+else:
+    print("NO \"name\" substring in the input text.", "\n")
+
+# Question 06
+# Program to check whether the given input lengths can form a triangle or not.
+
+len1 = int(input("\nEnter the first side length of triangle (integer) : "))
+len2 = int(input("Enter the second side length of triangle (integer) : "))
+len3 = int(input("Enter the third side length of triangle (integer) : "))
+
+if (len1 + len2 <= len3) or (len2 + len3 <= len1) or (len3 + len1 <= len2):
+    print("\nNO Triangle can be formed with such side lengths.")
+
+else:
+    print("\nYES the triangle with such sides can be formed.")
+
+
+
+
