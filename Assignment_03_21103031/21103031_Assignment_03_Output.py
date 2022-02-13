2e7c14016a6c9caaa0fd3cf599a2b014e8377f4f
AnshRajSharma <97468572+AnshRajSharma@users.noreply.github.com>
2022-02-13 23:49:59 +0530

    Add files via upload

---
+++ Assignment_03_21103031/21103031_Assignment_03_Output.py
@@ -0,0 +1,167 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_1.py ====================================================
+Enter some text : This is a python program and This python program counts occurrences
+The occurrences of 'a' is 1.
+The occurrences of 'is' is 1.
+The occurrences of 'program' is 2.
+The occurrences of 'occurrences' is 1.
+The occurrences of 'python' is 2.
+The occurrences of 'and' is 1.
+The occurrences of 'This' is 2.
+The occurrences of 'counts' is 1.
+
+Do you want to continue? (Y/N): Y
+Enter some text : Occurrences
+The occurrences of characters in entered single word are:
+The occurrences of 'c' is 3.
+The occurrences of 's' is 1.
+The occurrences of 'e' is 2.
+The occurrences of 'r' is 2.
+The occurrences of 'O' is 1.
+The occurrences of 'u' is 1.
+The occurrences of 'n' is 1.
+
+Do you want to continue? (Y/N): N
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_2.py ====================================================
+Enter day: 28
+Enter month: 2
+Enter year: 2000
+Next Date is: 29/2/2000
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_3.py ====================================================
+Enter the number of values to be evaluated: 3
+Enter Number_1 (integer): 3
+Enter Number_2 (integer): 9
+Enter Number_3 (integer): 10
+The inputs given are:  [3, 9, 10]
+The required output is:  [(3, 9), (9, 81), (10, 100)]
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_4.py ====================================================
+Enter the grade points of the student : 10
+The student's record: 
+	Your Grade is 'A+' and Outstanding Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 9
+The student's record: 
+	Your Grade is 'A' and Excellent Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 8
+The student's record: 
+	Your Grade is 'B+' and Very Good Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 7
+The student's record: 
+	Your Grade is 'B' and Good Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 6
+The student's record: 
+	Your Grade is 'C+' and Average Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 5
+The student's record: 
+	Your Grade is 'C' and Below Average Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 4
+The student's record: 
+	Your Grade is 'D' and Poor Performance.
+
+Do you want to check next Record? (Y/N): Y
+Enter the grade points of the student : 3
+ERROR! The grade points must be an integer from 4 to 10
+Enter the grade points of the student: 11
+ERROR! The grade points must be an integer from 4 to 10
+Enter the grade points of the student: 10
+The student's record: 
+	Your Grade is 'A+' and Outstanding Performance.
+
+Do you want to check next Record? (Y/N): N
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_5.py ====================================================
+The required pattern is:
+     ABCDEFGHIJK
+      ABCDEFGHI
+       ABCDEFG
+        ABCDE
+         ABC
+          A
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_6.py ====================================================
+Enter Student ID: 10001
+Enter Student Name: abcd
+Do you want to create a new student record? (Y/N): Y
+Enter Student ID: 10003
+Enter Student Name: ijkl
+Do you want to create a new student record? (Y/N): Y
+Enter Student ID: 10002
+Enter Student Name: qrst
+Do you want to create a new student record? (Y/N): Y
+Enter Student ID: 10004
+Enter Student Name: efgh
+Do you want to create a new student record? (Y/N): Y
+Enter Student ID: 10005
+Enter Student Name: mnop
+Do you want to create a new student record? (Y/N): N
+
+a.The students details are:
+ {'SID': 10001, 'Name': 'abcd'}
+ {'SID': 10003, 'Name': 'ijkl'}
+ {'SID': 10002, 'Name': 'qrst'}
+ {'SID': 10004, 'Name': 'efgh'}
+ {'SID': 10005, 'Name': 'mnop'}
+
+b.Sorted output using student name:
+ {'SID': 10001, 'Name': 'abcd'}
+ {'SID': 10004, 'Name': 'efgh'}
+ {'SID': 10003, 'Name': 'ijkl'}
+ {'SID': 10005, 'Name': 'mnop'}
+ {'SID': 10002, 'Name': 'qrst'}
+
+c.Sorted output using SID:
+ {'SID': 10001, 'Name': 'abcd'}
+ {'SID': 10002, 'Name': 'qrst'}
+ {'SID': 10003, 'Name': 'ijkl'}
+ {'SID': 10004, 'Name': 'efgh'}
+ {'SID': 10005, 'Name': 'mnop'}
+
+d.Enter the SID for search: 10008
+ERROR!! SID entered is not there in dictionary.
+Do you want to enter SID again? (Y/N): Y
+
+d.Enter the SID for search: 10002
+The name of the student is: qrst
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_7.py ====================================================
+Enter the number of terms: 10
+The required Fibonacci sequence is:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+The sum of terms of Fibonacci sequence is:  88
+The average of Fibonacci series: 8.80
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_03\Question_8.py ====================================================
+Given set1: {1, 2, 3, 4, 5}
+Given set2: {8, 2, 4, 6}
+Given set3: {1, 17, 5, 9, 13} 
+
+a.The set of all elements that are in Set1 and Set2 but not both is: {1, 3, 5, 6, 8}
+b.The set of all elements that are in only one of the three sets: {17, 3, 6, 8, 9, 13}
+c.The set of all elements that are exactly in two of the three sets: {1, 2, 4, 5}
+d.The set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e.The set of all integers in the range 1 to 10 that are not in all three sets: {10, 7}
