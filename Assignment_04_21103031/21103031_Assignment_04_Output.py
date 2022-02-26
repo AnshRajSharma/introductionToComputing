a5bdb090c5bff35da137b5fa32028876296ef63e
AnshRajSharma <97468572+AnshRajSharma@users.noreply.github.com>
2022-02-26 18:03:04 +0530

    Add files via upload

---
+++ Assignment_04_21103031/21103031_Assignment_04_Output.py
@@ -0,0 +1,120 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_1.py ====================================================
+Enter the number of disks: 3
+Move disk 1 from A to C.
+Move disk 2 from A to B
+Move disk 1 from C to B.
+Move disk 3 from A to C
+Move disk 1 from B to A.
+Move disk 2 from B to C
+Move disk 1 from A to C.
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_2.py ====================================================
+USING RECURSIVE PROCEDURES
+Enter the number of rows (Positive Integer) : 5
+The Pascal Triangle of 5 rows is :
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+USING ITERATIVE APPROACH 'for' LOOP
+Enter the number of rows (Positive integer) : 5
+The Pascal Triangle of 5 rows is :
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_3.py ====================================================
+Enter dividend (positive integer) : 20
+Enter divisor (positive integer) : 16
+The quotient is : 1
+The remainder is : 4
+
+a. Checking whether function/values is callable or not.
+Check the source code to see which value and function is used in callable method.
+False
+True
+
+b. Checking whether all the values are non zeros or not
+The previous output is : {1: 'Quotient', 4: 'Remainder'}
+The output is : True
+The quotient and remainder are non zero values.
+
+c. Adding values (4, 5, 6) to the result and filtering out the values which are greater than 4.
+The required filtered values in list form is : [5, 6, 7, 8, 9, 10]
+
+d. Converting the above list into set datatype.
+The required output is : {5, 6, 7, 8, 9, 10}
+
+e. Making above set immutable.
+The immutable set is : frozenset({5, 6, 7, 8, 9, 10})
+
+f. Finding maximum value from set and its hash value.
+(1) The maximum value from the set is : 10
+(2.a) The hash value of 10 is: 10
+(2.b) The hash value of frozenset({5, 6, 7, 8, 9, 10}) is: 2627905469991904590
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_4.py ====================================================
+Student_1 details are as follow: 
+Name : ABC 	 Roll Number : 1001
+
+Student_2 details are as follow: 
+Name : DEF 	 Roll Number : 1002
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_5.py ====================================================
+The total employee are : 3
+The employee details are :
+Name : Mehak , Salary : 40000
+Name : Ashok , Salary : 50000
+Name : Viren , Salary : 60000
+
+(a) The updated details are:
+Name : Mehak , Salary : 70000
+Name : Ashok , Salary : 50000
+Name : Viren , Salary : 60000
+
+(b) The modified records are:
+Name : Mehak , Salary : 70000
+Name : Ashok , Salary : 50000
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_04\Question_6.py ====================================================
+Welcome to the game of 'TEST THE FRIENDSHIP'.
+Let's begin...
+It's a two player game.
+Select your player one will be 'George' and other will be 'Barbie'.
+
+George, it's Your Turn.
+Enter a word George (lowercase) : end
+
+Barbie, Your Turn...
+Barbie you need to create a new word using the exact same letters as George.
+Enter a word Barbie (lowercase) : den
+
+You are True Friends.
+As you entered word that matches one of the combinations given here:
+['end', 'edn', 'ned', 'nde', 'dne', 'den']
+
+Do you want to play again? (Y/N) : Y
+Welcome to the game of 'TEST THE FRIENDSHIP'.
+Let's begin...
+It's a two player game.
+Select your player one will be 'George' and other will be 'Barbie'.
+
+George, it's Your Turn.
+Enter a word George (lowercase) : dna
+
+Barbie, Your Turn...
+Barbie you need to create a new word using the exact same letters as George.
+Enter a word Barbie (lowercase) : add
+
+Your friendship is a fake.
+Barbie, your word didn't match any combination from the list given here:
+['dna', 'dan', 'nda', 'nad', 'and', 'adn']
+
+Do you want to play again? (Y/N) : N
