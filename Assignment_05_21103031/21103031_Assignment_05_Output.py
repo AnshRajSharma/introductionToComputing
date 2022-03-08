4583e79602e1d71cb2e49b5962d7a0cf8944800d
AnshRajSharma <97468572+AnshRajSharma@users.noreply.github.com>
2022-03-08 20:14:34 +0530

    Add files via upload

---
+++ Assignment_05_21103031/21103031_Assignment_05_Output.py
@@ -0,0 +1,53 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_05\Question_1-3.py ===================================================
+Questions 01 to 03 are GUI based Questions
+and the programs are written by using the Tkinter
+Library for Python.
+
+Tkinter is the standard GUI library
+for Python. Python when combined with Tkinter provides a
+fast and easy way to create GUI applications. Tkinter provides
+a powerful object-oriented interface to the Tk GUI toolkit.
+
+The Outputs of the Questions can be seen by
+running the corresponding source codes as given in the
+source file on my github under Assignment 05 folder.
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_05\Question_4.py ====================================================
+Enter the number of students: 4
+Enter the marks of students in this Subject:
+Enter marks of STUDENT 1: 23
+Enter marks of STUDENT 2: 12
+Enter marks of STUDENT 3: 33
+Enter marks of STUDENT 4: 34
+Marks list entered by the user: [23, 12, 33, 34]
+Marks after required sorting: [12, 23, 33, 34]
+
+==================================================== RESTART: C:\Users\anshr\Desktop\python_Assignments\Assignment_05\Question_5.py ====================================================
+Enter the length of array: 11
+Enter only integer values:
+Enter INTEGER 1: 4
+Enter INTEGER 2: 5
+Enter INTEGER 3: 2
+Enter INTEGER 4: 4
+Enter INTEGER 5: 5
+Enter INTEGER 6: 6
+Enter INTEGER 7: 2
+Enter INTEGER 8: 3
+Enter INTEGER 9: 4
+Enter INTEGER 10: 5
+Enter INTEGER 11: 7
+
+The Input Array: [4, 5, 2, 4, 5, 6, 2, 3, 4, 5, 7]
+
+a. The Sorted Array: [2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]
+
+b. Enter the integer value to search for: 4
+The element 4 is present in the Sorted Array at indices:
+	3
+	4
+	5
+
+c. The number of occurrences of element 4 is: 3
